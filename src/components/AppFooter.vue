b2969bcaa512bc5e96e721ba51b3389aee383167
StefanoAntonelli93 <ste.antonelli@outlook.it>
2024-06-01 17:44:16 +0200

    [UPD] footer done

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,18 +10,21 @@ export default {
     <footer>
         <div class="container">
             <div>
-                <p>title</p>
-                <p>testoooooooooooooooooooooo
-
-                </p>
+                <p class="title">Boolando srl</p>
+                <ul>
+                    <li><a href="">Informazioni legali</a></li>
+                    <li><a href="">Informativa sulla privacy</a></li>
+                    <li><a href="">Diritti di recesso</a></li>
+                </ul>
             </div>
             <div>
+                <p>Trovaci anche su</p>
                 <ul>
-                    <li>icona</li>
-                    <li>icona</li>
-                    <li>icona</li>
-                    <li>icona</li>
-                    <li>icona</li>
+                    <li><a href=""><i class="fa-brands fa-square-twitter"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-square-facebook"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-square-instagram"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-square-pinterest"></i></a></li>
+                    <li><a href=""><i class="fa-brands fa-square-youtube"></i></a></li>
                 </ul>
             </div>
         </div>
@@ -39,6 +42,29 @@ footer {
         display: flex;
         color: $text-footer-color;
         justify-content: space-between;
+        padding: 30px 0;
+
+        .title {
+            font-weight: 700;
+            font-size: 20px;
+        }
+
+        ul {
+            display: flex;
+            justify-content: space-between;
+            padding-top: 10px;
+            gap: 15px;
+
+
+            a {
+                color: $text-footer-color;
+
+                i {
+                    font-size: 20px;
+                }
+
+            }
+        }
 
     }
 }
